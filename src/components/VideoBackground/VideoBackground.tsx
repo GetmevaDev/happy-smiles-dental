d674a95450ee6ef961052f961b1939a1ad31c327
GetmevaDev <devteam@getmeva.com>
2024-04-03 19:37:58 +0700

    muted

--- src/components/VideoBackground/VideoBackground.tsx
+++ src/components/VideoBackground/VideoBackground.tsx
@@ -12,6 +12,7 @@ const VideoBackground: FC<{ video: string; isFullScreen?: boolean; query?: boole
         controls={query || isFullScreen}
         controlsList='nodownload'
         loop
+        muted={!query}
         playsInline
         poster='/poster.jpg'
         preload='auto'
